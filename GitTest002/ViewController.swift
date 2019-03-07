64109840016a5771f58f78dc9ff46446d9a11f93
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-07 17:30:22 +0900

    add comment & change red color

--- GitTest002/ViewController.swift
+++ GitTest002/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.yellow
+        // background color red
+        self.view.backgroundColor = UIColor.red
     }
 
 
