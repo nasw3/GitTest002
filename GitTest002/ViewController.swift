c88b93f42cf32124116be12d53ef27097e11574f
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-07 17:09:14 +0900

    Initial Commit

---
+++ GitTest002/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest002
+//
+//  Created by D7702_10 on 2019. 3. 7..
+//  Copyright © 2019년 Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
